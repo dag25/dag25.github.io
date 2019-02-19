e77e50f3c5689837ae3420196067889f8f96bc80
DAG <dag25@mail.ru>
2019-02-19 17:14:29 +0300

    last

---
+++ homework2/js/script.js
@@ -0,0 +1,29 @@
+
+    var video = document.querySelector("#video"),
+        button = document.querySelector("a");
+    
+    button.addEventListener("click", function() {
+      video.play()
+      video.setAttribute("controls","controls");
+    }, false);
+    
+    $(".video-play-btn").click(function() {
+      $(".video-play-btn").addClass('video-play-btn--hide')
+    })
+
+
+
+
+
+
+/*var video = document.querySelector("#video"),
+    button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    video.play()
+    video.setAttribute("controls", "controls");
+}, false);
+
+$(".video-play-btn").click(function () {
+    $(".video-play-btn").addClass('video-play-btn--hide')
+});*/
