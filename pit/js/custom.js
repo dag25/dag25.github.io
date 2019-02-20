d44c25055e0afd2ef65494670b24d776a977ccce
DAG <dag25@mail.ru>
2019-02-20 16:59:37 +0300

    pit

---
+++ pit/js/custom.js
@@ -0,0 +1,44 @@
+$(function(){
+
+	var hf = function(){
+		var h_header = $('header').height();
+		var h_footer = $('footer').height();
+		$('.content').css({
+			'paddingTop': h_header,
+			'paddingBottom': h_footer
+		});
+	}
+
+	$(window).bind('load resize', hf);
+
+});
+
+
+
+
+
+/* portfolio section */
+$(function(){
+	$('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false;
+	});
+	// slick
+	$('.banner-slider, .testimonial-slider').slick({
+		arrows: false,
+		dots: true,
+	});
+	$('.portfolio-slider').slick({
+		dots: true,
+		appendArrows:'.portfolio-slider__buttons',
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>'
+	});
+});
+
+
+
